932319d7b3d7f286f84dd5a58f63201717fa7bf8
Marcos Balatka <m.balatka5@gmail.com>
2018-02-28 22:25:35 -0300

    Força de colisão v1

--- airHockey.js
+++ airHockey.js
@@ -1,9 +1,10 @@
-var p1, disk;
+var p1, disk, pause;
  
 function setup() {
   createCanvas(800, 500);
   p1 = new Player();
   disk = new Disk();
+  pause = false;
 }
  
 function draw() {
@@ -25,10 +26,11 @@ function Disk() {
   this.angle = 0.5;
   this.radius = 25;
   this.decSpd = 0.001;
+  this.bounce = 0.9;
  
   this.move = function() {
     if(this.speed > 0) {
-      this.speed -= this.decSpd;
+      this.speed -= this.speed*this.decSpd;
     }
     if(this.speed <=0) {
       this.speed = 0;
@@ -57,17 +59,6 @@ function Disk() {
     line(this.x, this.y, this.x+cos(this.angle)*100, this.y+sin(this.angle)*100);
   }
  
-  this.collisionOld = function() {
-    p = p1;
-    if(sqrt(((this.x-p.x)*(this.x-p.x)) + ((this.y-p.y)*(this.y-p.y))) < (this.radius + p.radius)/2) {
-      this.angle += PI/2;
-      this.speed = 5;
-    }
-    while(sqrt(((this.x-p.x)*(this.x-p.x)) + ((this.y-p.y)*(this.y-p.y))) < (this.radius + p.radius)/2) {
-      this.move();
-    }
-  }
- 
   this.reflect = function(wAng){
     var aIn, aOut, diff;
     aIn = this.angle + PI;
@@ -80,42 +71,22 @@ function Disk() {
     p = p1;
     if(collisionP1Disk()) {
       this.angle = this.reflect(atan2(p.y - this.y, p.x - this.x) +PI/2);
-      this.speed = 5;
+      this.speed = this.collisionForce() * this.bounce;
     }
     while(collisionP1Disk()) {
       this.x += this.speed * cos(this.angle);
       this.y += this.speed * sin(this.angle);
     }
   }
- 
-  /*this.collision = function() {
-    p = p1;
-    if(sqrt(((this.x-p.x)*(this.x-p.x)) + ((this.y-p.y)*(this.y-p.y))) < (this.radius + p.radius)/2) {
-      var vxP1, vyP1, vxD, vYD, vxR, vyR;
-      vxP1 = cos(p.getAngle()) * p.getSpeed();
-      vyP1 = sin(p.getAngle()) * p.getSpeed();
-      vxD = cos(this.angle) * this.speed;
-      vyD = sin(this.angle) * this.speed;
-      vxR = vxP1 + vxD - this.x;
-      vyR = vyP1 + vyD - this.y;
-      print("1|"+vxP1+"|");
-      print("2|"+vyP1+"|");
-      print("3|"+vxD+"|");
-      print("4|"+vyD+"|");
-      print("5|"+vxR+"|");
-      print("6|"+vyR+"|");
- 
-      this.angle = atan2(vyR, vxR);
-      debugPrints();
-      noLoop();
-      //this.angle += PI/2;//mudar
-      this.speed = 5;//mudar
-      //background(255, 255, 0);
-    }
-    while(sqrt(((this.x-p.x)*(this.x-p.x)) + ((this.y-p.y)*(this.y-p.y))) < (this.radius + p.radius)/2) {
-      this.move();
-    }
-  }*/
+  
+  this.collisionForce = function() {
+    var vxP1, vyP1, vxD, vyD;  
+    vxP1 = cos(p1.getAngle()) * p1.getSpeed();
+    vyP1 = sin(p1.getAngle()) * p1.getSpeed();
+    vxD = cos(this.angle) * this.speed;
+    vyD = sin(this.angle) * this.speed;
+    return sqrt((vxP1 - vxD)*(vxP1 - vxD) + (vyP1 - vyD)*(vyP1 - vyD));
+  }
 };
  
 function Player() {
@@ -126,6 +97,7 @@ function Player() {
   this.radius = 50;
  
   this.update = function() {
+    var iniSpd = this.getSpeed2();
     var dist = sqrt(((this.x-mouseX)*(this.x-mouseX)) + ((this.y-mouseY)*(this.y-mouseY)));
     var angl = atan2(this.x-mouseX, this.y-mouseY);
     if(dist > 2) {
@@ -140,7 +112,7 @@ function Player() {
         this.y += dy/qtd;
         while(collisionP1Disk()) {
           disk.angle = atan2(disk.y-this.y, disk.x-this.x);
-          disk.speed = 5;
+          disk.speed = (iniSpd/2)*disk.bounce;
           disk.x = this.x+cos(disk.angle)*((disk.radius + this.radius)/2+1);
           disk.y = this.y+sin(disk.angle)*((disk.radius + this.radius)/2+1);
         }
@@ -172,16 +144,21 @@ function Player() {
   this.getSpeed = function() {
     return sqrt(((this.x-this.old_x)*(this.x-this.old_x)) + ((this.y-this.old_y)*(this.y-this.old_y)));
   }
+  
+  this.getSpeed2 = function() {
+    return sqrt(((mouseX-this.x)*(mouseX-this.x)) + ((mouseY-this.y)*(mouseY-this.y)));
+  }
 };
  
 function mousePressed() {
-  noLoop();
+  if(pause) {
+    pause = false;
+    loop();
+  }else {
+    pause = true;
+    noLoop();
+  }
 }
-/*
-function mouseReleased() {
-  loop();
-}
-*/
  
 function collisionP1Disk() {
   if(sqrt(((disk.x-p1.x)*(disk.x-p1.x)) + ((disk.y-p1.y)*(disk.y-p1.y))) < (disk.radius + p1.radius)/2) {
